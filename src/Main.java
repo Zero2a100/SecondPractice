875a09ed33b18f9b93c887368cfbfa172de2afcb
Zero2a100 <yvanrodrigomj@gmail.com>
2023-11-26 14:40:49 -0500

    SecondPractice

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,15 @@ public class Main {
         Scanner keyboard = new Scanner(System.in).useDelimiter("\\n");
         System.out.println("Enter Name");
         String myName = keyboard.next();
-        System.out.println("Enter last name");
-        String lastName = keyboard.next();
-        System.out.println("Enter address");
-        String myAddress = keyboard.next();
-        System.out.println("My name is " + myName + " " + lastName + " and my address is " + myAddress);
+        //System.out.println("Enter last name");
+        //String lastName = keyboard.next();
+        //System.out.println("Enter address");
+        //String myAddress = keyboard.next();
+        //System.out.println("My name is " + myName + " " + lastName + " and my address is " + myAddress);
+        for (int i = 0; i <= myName.length() - 1; i++) {
+            System.out.println(myName.charAt(i));
+            //for (int ;= myName.charAt(0); int <= myName.length())
+        }
+        System.out.println(myName.length());
     }
 }
